b4aba50c0cbe00579c3d9d09ba710b7ccfda4cba
kneskoromny <kneskoromny@gmail.com>
2022-02-23 19:36:07 +0300

    Initial Commit

---
+++ H4xorNewsSwiftUI/H4xorNewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4xorNewsSwiftUIApp.swift
+//  H4xorNewsSwiftUI
+//
+//  Created by Кирилл Нескоромный on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4xorNewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
